d65550d3194b760c14ac009065f6c962c75516f2
Richie Bendall <richiebendall@gmail.com>
2020-03-05 18:57:06 +1300

    feat: Link snap install url
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- install.js
+++ install.js
@@ -40,8 +40,8 @@ module.exports = (async () => {
 	if (resolved) return
 
 	let message = `Unable to find a suitable VLC binary for you current OS. Please ${terminalLink("install VLC", "https://www.videolan.org/vlc/#download")}.`
-	if (platform === "linux") message = `Unable to find a suitable VLC binary for Linux. If you have snapcraft installed, run ${chalk.grey("sudo snap install vlc")}.`
-	if (platform === "darwin") message = `Unable to find a suitable VLC binary for MacOS. Please ${terminalLink("install VLC", "https://www.videolan.org/vlc/download-macosx.html")}.`
+	if (platform === "linux") message = `Unable to find a suitable VLC binary for Linux. If you have snapcraft installed, run ${terminalLink(chalk.grey("sudo snap install vlc"), "snap://vlc", { fallback: false })}.`
+	if (platform === "darwin") message = `Unable to find a suitable VLC binary for MacOS.Please ${terminalLink("install VLC", "https://www.videolan.org/vlc/download-macosx.html")}.`
 
 	return console.log(boxen(message, { padding: 1, borderColor: "yellow" }))
 })()
